94e568e392cb8cb9d4569758e6ec5e0d1c9fd4c1
valery <ebalaika1994@gmail.com>
2022-05-04 12:22:56 +0300

    make transactions

--- go.mod
+++ go.mod
@@ -5,9 +5,23 @@ go 1.17
 require (
 	github.com/briandowns/openweathermap v0.16.0
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
-	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jackc/pgx/v4 v4.16.0
 	github.com/joho/godotenv v1.4.0
+)
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.12.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.11.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/puddle v1.2.1 // indirect
 	github.com/lib/pq v1.10.5 // indirect
+	github.com/pashagolub/pgxmock v1.5.0 // indirect
 	github.com/spf13/viper v1.10.1 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- storage/storage.go
+++ storage/storage.go
@@ -2,7 +2,6 @@ package storage
 
 import (
 	"context"
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/jackc/pgx/v4"
 	"time"
@@ -12,52 +11,66 @@ type Storage struct {
 	*pgx.Conn
 }
 
-func NewStorage(db *pgx.Conn) *Storage {
-	return &Storage{db}
+func NewStorage(conn *pgx.Conn) *Storage {
+	return &Storage{conn}
 }
 
-func (s *Storage) SaveUserMessage(msg tgbotapi.Message, answer string) {
-	s.saveUser(msg)
-	s.saveMessage(msg, answer)
-}
+func (s *Storage) SaveUserMessage(msg tgbotapi.Message, answer string) error {
+	tx, err := s.BeginTx(context.Background(), pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
 
-func (s *Storage) SaveUserLocation(msg tgbotapi.Message, answer string) {
-	s.saveUser(msg)
-	s.saveLocation(msg, answer)
-}
-
-func (s *Storage) saveUser(msg tgbotapi.Message) {
-	_, err := s.Exec(context.Background(),
+	_, err = s.Exec(context.Background(),
 		"INSERT INTO users"+
 			" (chat_id, first_name, last_name, user_name, language_code)"+
 			"VALUES ($1, $2, $3, $4, $5)"+
 			"ON CONFLICT (chat_id) DO NOTHING;",
 		msg.Chat.ID, msg.From.FirstName, msg.From.LastName, msg.From.UserName, msg.From.LanguageCode)
 	if err != nil {
-		fmt.Println(err)
+		tx.Rollback(context.Background())
+		return err
 	}
 
-}
-
-func (s *Storage) saveMessage(msg tgbotapi.Message, answer string) {
-	_, err := s.Exec(context.Background(),
+	_, err = s.Exec(context.Background(),
 		"INSERT INTO message"+
 			" (chat_id, msg_text, receive_at, response_text, response_at)"+
 			"VALUES ($1, $2, $3, $4, $5);",
 		msg.Chat.ID, msg.Text, msg.Date, answer, time.Now().Unix())
 	if err != nil {
-		fmt.Println(err)
+		tx.Rollback(context.Background())
+		return err
 	}
+	err = tx.Commit(context.Background())
+	return err
 }
 
-func (s *Storage) saveLocation(msg tgbotapi.Message, answer string) {
-	_, err := s.Exec(context.Background(),
+func (s *Storage) SaveUserLocation(msg tgbotapi.Message, answer string) error {
+	tx, err := s.BeginTx(context.Background(), pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	_, err = s.Exec(context.Background(),
+		"INSERT INTO users"+
+			" (chat_id, first_name, last_name, user_name, language_code)"+
+			"VALUES ($1, $2, $3, $4, $5)"+
+			"ON CONFLICT (chat_id) DO NOTHING;",
+		msg.Chat.ID, msg.From.FirstName, msg.From.LastName, msg.From.UserName, msg.From.LanguageCode)
+	if err != nil {
+		tx.Rollback(context.Background())
+		return err
+	}
+
+	_, err = s.Exec(context.Background(),
 		"INSERT INTO message"+
 			" (chat_id, msg_text, longitude, latitude, receive_at, response_text, response_at)"+
 			"VALUES ($1, $2, $3, $4, $5, $6, $7);",
 		msg.Chat.ID, msg.Text, msg.Location.Longitude, msg.Location.Latitude, msg.Date, answer, time.Now().Unix())
 	if err != nil {
-		fmt.Println(err)
+		tx.Rollback(context.Background())
+		return err
 	}
-
+	err = tx.Commit(context.Background())
+	return err
 }
